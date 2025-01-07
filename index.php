2e5e0f2afb2c867335a81343121d23e245264e69
Taleh Muzaffarov <talehmuzaffer@gmail.com>
2025-01-07 01:18:12 +0100

    google tags

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6KVQ7Z3YN6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6KVQ7Z3YN6');
+</script>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
+	<meta name="google-adsense-account" content="ca-pub-4645247097169910">
+	<meta name="p:domain_verify" content="c5722b4397f081632ef139517f94e664"/>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4645247097169910"
+     crossorigin="anonymous"></script>
 </head>
 <body <?php body_class(); ?>>
 
